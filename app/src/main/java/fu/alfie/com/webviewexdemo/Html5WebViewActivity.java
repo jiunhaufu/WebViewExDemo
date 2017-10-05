e107d431d941ac8e2f01d2a15ac53fcd5a94c460
傅俊皓 <alfie.fu@toplogis.com>
2017-10-05 10:40:48 +0800

    增加html5 WebView

---
+++ app/src/main/java/fu/alfie/com/webviewexdemo/Html5WebViewActivity.java
@@ -0,0 +1,305 @@
+package fu.alfie.com.webviewexdemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+
+import java.io.File;
+
+public class Html5WebViewActivity extends AppCompatActivity {
+
+    private String mUrl;
+
+    private LinearLayout mLayout;
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+        setContentView(R.layout.activity_html5_web_view);
+
+        Intent intent = getIntent();
+        String inputUrl = intent.getStringExtra("inputUrl");
+
+        mLayout = (LinearLayout) findViewById(R.id.web_layout);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams
+                .MATCH_PARENT);
+        mWebView = new Html5WebView(getApplicationContext());
+        mWebView.setLayoutParams(params);
+        mLayout.addView(mWebView);
+
+        mWebView.loadUrl(inputUrl);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.close:
+                Html5WebViewActivity.this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private long mOldTime;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (System.currentTimeMillis() - mOldTime < 1500) {
+                mWebView.clearHistory();
+                mWebView.loadUrl(mUrl);
+            } else if (mWebView.canGoBack()) {
+                mWebView.goBack();
+            } else {
+                Html5WebViewActivity.this.finish();
+            }
+            mOldTime = System.currentTimeMillis();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mWebView != null) {
+            mWebView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            mWebView.clearHistory();
+
+            ((ViewGroup) mWebView.getParent()).removeView(mWebView);
+            mWebView.destroy();
+            mWebView = null;
+        }
+        super.onDestroy();
+    }
+}
+
+class Html5WebView extends WebView {
+
+    private Context mContext;
+
+    public Html5WebView(Context context) {
+        super(context);
+        mContext = context;
+        init();
+    }
+
+    public Html5WebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Html5WebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init() {
+        WebSettings mWebSettings = getSettings();
+        mWebSettings.setSupportZoom(true);
+        mWebSettings.setLoadWithOverviewMode(true);
+        mWebSettings.setUseWideViewPort(true);
+        mWebSettings.setDefaultTextEncodingName("utf-8");
+        mWebSettings.setLoadsImagesAutomatically(true);
+
+        //调用JS方法.安卓版本大于17,加上注解 @JavascriptInterface
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setSupportMultipleWindows(true);
+        //缓存数据
+        saveData(mWebSettings);
+        newWin(mWebSettings);
+        setWebChromeClient(webChromeClient);
+        setWebViewClient(webViewClient);
+    }
+
+    /**
+     * 多窗口的问题
+     */
+    private void newWin(WebSettings mWebSettings) {
+        //html中的_bank标签就是新建窗口打开，有时会打不开，需要加以下
+        //然后 复写 WebChromeClient的onCreateWindow方法
+        mWebSettings.setSupportMultipleWindows(false);
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+    }
+
+    /**
+     * HTML5数据存储
+     */
+    private void saveData(WebSettings mWebSettings) {
+        //有时候网页需要自己保存一些关键数据,Android WebView 需要自己设置
+
+        if (NetStatusUtil.isConnected(mContext)) {
+            mWebSettings.setCacheMode(WebSettings.LOAD_DEFAULT);//根据cache-control决定是否从网络上取数据。
+        } else {
+            mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);//没网，则从本地获取，即离线加载
+        }
+        File cacheDir = mContext.getCacheDir();
+        if (cacheDir != null){
+            String appCachePath  = cacheDir.getAbsolutePath();
+            mWebSettings.setDomStorageEnabled(true);
+            mWebSettings.setDatabaseEnabled(true);
+            mWebSettings.setAppCacheEnabled(true);
+            mWebSettings.setAppCachePath(appCachePath);
+        }
+    }
+
+    WebViewClient webViewClient = new WebViewClient() {
+        /**
+         * 多页面在同一个WebView中打开，就是不新建activity或者调用系统浏览器打开
+         */
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            Log.d("Url:", url);
+            return true;
+        }
+    };
+
+    WebChromeClient webChromeClient = new WebChromeClient() {
+
+        //=========HTML5定位==========================================================
+        //需要先加入权限
+        //<uses-permission android:name="android.permission.INTERNET"/>
+        //<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
+        //<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
+        @Override
+        public void onReceivedIcon(WebView view, Bitmap icon) {
+            super.onReceivedIcon(view, icon);
+        }
+
+        @Override
+        public void onGeolocationPermissionsHidePrompt() {
+            super.onGeolocationPermissionsHidePrompt();
+        }
+
+        @Override
+        public void onGeolocationPermissionsShowPrompt(final String origin, final GeolocationPermissions.Callback callback) {
+            callback.invoke(origin, true, false);//注意个函数，第二个参数就是是否同意定位权限，第三个是是否希望内核记住
+            super.onGeolocationPermissionsShowPrompt(origin, callback);
+        }
+        //=========HTML5定位==========================================================
+
+
+        //=========多窗口的问题==========================================================
+        @Override
+        public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+            WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+            transport.setWebView(view);
+            resultMsg.sendToTarget();
+            return true;
+        }
+        //=========多窗口的问题==========================================================
+    };
+}
+
+class NetStatusUtil {
+
+    /**
+     * 检测当前打开的网络类型是否WIFI
+     *
+     * @param context 上下文
+     * @return 是否是Wifi上网
+     */
+    public static boolean isWifi(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context
+                .CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        if (activeNetInfo != null && activeNetInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 是否有网
+     */
+    public static boolean isConnected(Context context) {
+        return netSatus(context) != NetState.NET_NO;
+    }
+
+    /**
+     * 枚举网络状态 NET_NO：没有网络 NET_2G:2g网络 NET_3G：3g网络 NET_4G：4g网络 NET_WIFI：wifi
+     * NET_UNKNOWN：未知网络
+     */
+    public static enum NetState {
+        NET_NO, NET_2G, NET_3G, NET_4G, NET_WIFI, NET_UNKNOWN
+    }
+
+    /**
+     * 判断当前是否网络连接
+     *
+     * @param context 上下文
+     * @return 状态码
+     */
+    public static NetState netSatus(Context context) {
+        NetState stateCode = NetState.NET_NO;
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        if (ni != null && ni.isConnectedOrConnecting()) {
+            switch (ni.getType()) {
+                case ConnectivityManager.TYPE_WIFI:
+                    stateCode = NetState.NET_WIFI;
+                    break;
+                case ConnectivityManager.TYPE_MOBILE:
+                    switch (ni.getSubtype()) {
+                        case TelephonyManager.NETWORK_TYPE_GPRS: // 联通2g
+                        case TelephonyManager.NETWORK_TYPE_CDMA: // 电信2g
+                        case TelephonyManager.NETWORK_TYPE_EDGE: // 移动2g
+                        case TelephonyManager.NETWORK_TYPE_1xRTT:
+                        case TelephonyManager.NETWORK_TYPE_IDEN:
+                            stateCode = NetState.NET_2G;
+                            break;
+                        case TelephonyManager.NETWORK_TYPE_EVDO_A: // 电信3g
+                        case TelephonyManager.NETWORK_TYPE_UMTS:
+                        case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                        case TelephonyManager.NETWORK_TYPE_HSDPA:
+                        case TelephonyManager.NETWORK_TYPE_HSUPA:
+                        case TelephonyManager.NETWORK_TYPE_HSPA:
+                        case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                        case TelephonyManager.NETWORK_TYPE_EHRPD:
+                        case TelephonyManager.NETWORK_TYPE_HSPAP:
+                            stateCode = NetState.NET_3G;
+                            break;
+                        case TelephonyManager.NETWORK_TYPE_LTE:
+                            stateCode = NetState.NET_4G;
+                            break;
+                        default:
+                            stateCode = NetState.NET_UNKNOWN;
+                    }
+                    break;
+                default:
+                    stateCode = NetState.NET_UNKNOWN;
+            }
+
+        }
+        return stateCode;
+    }
+}
--- app/src/main/java/fu/alfie/com/webviewexdemo/MainActivity.java
+++ app/src/main/java/fu/alfie/com/webviewexdemo/MainActivity.java
@@ -1,6 +1,10 @@
 package fu.alfie.com.webviewexdemo;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsClient;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,5 +34,22 @@ public class MainActivity extends AppCompatActivity {
     public void onFullscreenWebViewClick(View view){
         String inputUrl = String.valueOf(editText.getText());
         startActivity(new Intent(this,FullscreenWebViewActivity.class).putExtra("inputUrl",inputUrl));
+    }
+
+    public void onChromeCustomTabs(View view){
+        String inputUrl = String.valueOf(editText.getText());
+//        CustomTabsClient.connectAndInitialize(this, "com.android.chrome"); //简单的预启动要放在onStart
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder
+                .setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                .setShowTitle(true)
+                .build();
+        customTabsIntent.launchUrl(this, Uri.parse(inputUrl));
+
+    }
+
+    public void onHtml5WebViewClick(View view){
+        String inputUrl = String.valueOf(editText.getText());
+        startActivity(new Intent(this,Html5WebViewActivity.class).putExtra("inputUrl",inputUrl));
     }
 }
