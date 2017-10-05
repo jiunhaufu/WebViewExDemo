f305234fd49375603aa67b516718632dceca4ab2
傅俊皓 <alfie.fu@toplogis.com>
2017-10-05 18:39:35 +0800

    增加最佳解決方案的MyWebView

--- app/src/main/java/fu/alfie/com/webviewexdemo/MainActivity.java
+++ app/src/main/java/fu/alfie/com/webviewexdemo/MainActivity.java
@@ -1,14 +1,23 @@
 package fu.alfie.com.webviewexdemo;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,5 +60,95 @@ public class MainActivity extends AppCompatActivity {
     public void onHtml5WebViewClick(View view){
         String inputUrl = String.valueOf(editText.getText());
         startActivity(new Intent(this,Html5WebViewActivity.class).putExtra("inputUrl",inputUrl));
+    }
+
+    //=========================================MY=WEBVIEW===========================================
+
+    public void onMyWebViewClick(View view){
+        if (isConnected()){
+            String inputUrl = String.valueOf(editText.getText());  //跳轉網址
+            //ANDROID 5.0以上使用Webview
+            if (Build.VERSION.SDK_INT >= 21){
+                startActivity(new Intent(this,MyWebViewActivity.class).putExtra("inputUrl",inputUrl));
+            //ANDROID 5.0以下使用Chrome Custom Tab
+            }else{
+                //檢查是否安裝Chrome瀏覽器
+                PackageManager packageManager = getPackageManager();
+                if (isPackageInstalled("com.android.chrome",packageManager)){
+                    //開啟ChromeTab
+                    //compile 'com.android.support:customtabs:26.+'
+                    CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                    CustomTabsIntent customTabsIntent = builder
+                            .setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
+                            .setShowTitle(true)
+                            .build();
+                    customTabsIntent.launchUrl(this, Uri.parse(inputUrl));
+                //引導安裝ChromeTab
+                }else{
+                    new AlertDialog.Builder(this)
+                            .setIcon(R.mipmap.ic_launcher)
+                            .setTitle("瀏覽器版本不相容")
+                            .setMessage("Android 5.0以下手機請安裝Chrome瀏覽器")
+                            .setPositiveButton("前往安裝", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.android.chrome")));
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                            .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Toast.makeText(getApplicationContext(),"很抱歉~\n無法使用此應用服務",Toast.LENGTH_SHORT).show();
+                                    dialogInterface.cancel();
+                                }
+                            })
+                            .show();
+                }
+            }
+        }else{
+            new AlertDialog.Builder(this)
+                    .setIcon(R.mipmap.ic_launcher_round)
+                    .setTitle("連線異常")
+                    .setMessage("請檢查是否開啟網路")
+                    .setPositiveButton("開啟網路設定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    })
+                    .show();
+        }
+    }
+
+    //檢查網路連線是否正常
+    //<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+    private boolean isConnected(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+    //檢查是否安裝並啟用應用程式
+    private boolean isPackageInstalled(String packagename, PackageManager packageManager) {
+        try {
+            packageManager.getPackageInfo(packagename, 0);
+            if (packageManager.getApplicationEnabledSetting(packagename) == PackageManager.COMPONENT_ENABLED_STATE_ENABLED){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 }
---
+++ app/src/main/java/fu/alfie/com/webviewexdemo/MyWebViewActivity.java
@@ -0,0 +1,196 @@
+package fu.alfie.com.webviewexdemo;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyWebViewActivity extends AppCompatActivity {
+
+    WebView webView;
+    private static final String TAG = WebViewActivity.class.getSimpleName();
+    private String photoPath;
+    private ValueCallback<Uri> uriValueCallback;
+    private ValueCallback<Uri[]> uriValueCallbackArray;
+    private final static int FCR=1;
+
+    @SuppressLint({"SetJavaScriptEnabled", "WrongViewCast"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //設定全螢幕
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//隱藏title bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);//隱藏title bar
+        getSupportActionBar().hide();//隱藏action bar
+
+        setContentView(R.layout.activity_my_web_view);
+        //危險權限管理
+        if(Build.VERSION.SDK_INT >=23 &&
+          (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+           ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
+        }
+        //webview環境設定
+        webView = (WebView) findViewById(R.id.my_web_view);
+        WebSettings webSettings = webView.getSettings();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        if(Build.VERSION.SDK_INT >= 21){
+            webView.getSettings().setMixedContentMode(0);
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
+        webView.setWebViewClient(new Callback());
+        //取得待載入的網址連結
+        Intent intent = getIntent();
+        String inputUrl = intent.getStringExtra("inputUrl");
+        webView.loadUrl(inputUrl);
+        //webview環境設定
+        webView.setWebChromeClient(new WebChromeClient(){
+            //For Android 5.0+
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    WebChromeClient.FileChooserParams fileChooserParams){
+                if(uriValueCallbackArray != null){
+                    uriValueCallbackArray.onReceiveValue(null);
+                }
+                uriValueCallbackArray = filePathCallback;
+                //拍照
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(takePictureIntent.resolveActivity(MyWebViewActivity.this.getPackageManager()) != null){
+                    File photoFile = null;
+                    try{
+                        photoFile = createImageFile();
+                        takePictureIntent.putExtra("PhotoPath", photoPath);
+                    }catch(IOException ex){
+                        Log.e(TAG, "Image file creation failed", ex);
+                    }
+                    if(photoFile != null){
+                        photoPath = "file:" + photoFile.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                    }else{
+                        takePictureIntent = null;
+                    }
+                }
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);  //開啟文件
+                contentSelectionIntent.setType("*/*");
+                //錄影
+                Intent recordVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                //錄音
+                Intent recordAudioIntent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+                Intent[] intentArray;
+                if(takePictureIntent != null){
+                    intentArray = new Intent[]{takePictureIntent,recordVideoIntent,recordAudioIntent};
+                }else{
+                    intentArray = new Intent[0];
+                }
+
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "開啟應用程式");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                startActivityForResult(chooserIntent, FCR);
+                return true;
+            }
+        });
+    }
+
+    public class Callback extends WebViewClient {
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+            Toast.makeText(getApplicationContext(), "Failed loading app!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(Build.VERSION.SDK_INT >= 21){
+            Uri[] results = null;
+            //Check if response is positive
+            if(resultCode== Activity.RESULT_OK){
+                if(requestCode == FCR){
+                    if(null == uriValueCallbackArray){
+                        return;
+                    }
+                    if(intent == null){
+                        //Capture Photo if no image available
+                        if(photoPath != null){
+                            results = new Uri[]{Uri.parse(photoPath)};
+                        }
+                    }else{
+                        String dataString = intent.getDataString();
+                        if(dataString != null){
+                            results = new Uri[]{Uri.parse(dataString)};
+                        }
+                    }
+                }
+            }
+            uriValueCallbackArray.onReceiveValue(results);
+            uriValueCallbackArray = null;
+        }else{
+            if(requestCode == FCR){
+                if(null == uriValueCallback) return;
+                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                uriValueCallback.onReceiveValue(result);
+                uriValueCallback = null;
+            }
+        }
+    }
+
+    // Create an image file
+    private File createImageFile() throws IOException{
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img_"+timeStamp+"_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName,".jpg",storageDir);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, @NonNull KeyEvent event){
+        if(event.getAction() == KeyEvent.ACTION_DOWN){
+            switch(keyCode){
+                case KeyEvent.KEYCODE_BACK:
+                    if(webView.canGoBack()){
+                        webView.goBack();
+                    }else{
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+    }
+}
